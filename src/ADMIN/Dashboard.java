9e2f09886391234a3b31b6ffe3153aa89f3085fb
Phuc <22521141@gm.uit.edu.vn>
2024-06-03 23:44:17 +0700

    cai thien giao dien

--- src/ADMIN/Dashboard.java
+++ src/ADMIN/Dashboard.java
@@ -39,69 +39,28 @@ public class Dashboard extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        HomePanel = new javax.swing.JPanel();
         LbChao = new javax.swing.JLabel();
-        LbNgayThangNam = new javax.swing.JLabel();
         LbBuoiIcon = new javax.swing.JLabel();
+        LbNgayThangNam = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(255, 255, 255));
         setPreferredSize(new java.awt.Dimension(1055, 768));
-
-        HomePanel.setBackground(new java.awt.Color(255, 255, 255));
-        HomePanel.setPreferredSize(new java.awt.Dimension(1280, 655));
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         LbChao.setFont(new java.awt.Font("Arial", 1, 48)); // NOI18N
         LbChao.setText(getBuoi());
+        add(LbChao, new org.netbeans.lib.awtextra.AbsoluteConstraints(351, 95, -1, 66));
+
+        LbBuoiIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource(getBuoiIcon())));
+        add(LbBuoiIcon, new org.netbeans.lib.awtextra.AbsoluteConstraints(654, 80, -1, -1));
 
         LbNgayThangNam.setFont(new java.awt.Font("Arial", 2, 24)); // NOI18N
         LbNgayThangNam.setText(ngay[c.get(Calendar.DAY_OF_WEEK)]+", ngày " +c.get(Calendar.DATE)+" Tháng " + (c.get(Calendar.MONTH)+1)+" năm "+ c.get(Calendar.YEAR));
+        add(LbNgayThangNam, new org.netbeans.lib.awtextra.AbsoluteConstraints(351, 167, -1, -1));
 
-        LbBuoiIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource(getBuoiIcon())));
-
-        javax.swing.GroupLayout HomePanelLayout = new javax.swing.GroupLayout(HomePanel);
-        HomePanel.setLayout(HomePanelLayout);
-        HomePanelLayout.setHorizontalGroup(
-            HomePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(HomePanelLayout.createSequentialGroup()
-                .addGap(237, 237, 237)
-                .addGroup(HomePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(LbNgayThangNam)
-                    .addGroup(HomePanelLayout.createSequentialGroup()
-                        .addComponent(LbChao)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(LbBuoiIcon)))
-                .addContainerGap(391, Short.MAX_VALUE))
-        );
-        HomePanelLayout.setVerticalGroup(
-            HomePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(HomePanelLayout.createSequentialGroup()
-                .addGap(103, 103, 103)
-                .addGroup(HomePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(HomePanelLayout.createSequentialGroup()
-                        .addGap(15, 15, 15)
-                        .addComponent(LbChao, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(LbBuoiIcon))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(LbNgayThangNam)
-                .addContainerGap(473, Short.MAX_VALUE))
-        );
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(105, Short.MAX_VALUE)
-                .addComponent(HomePanel, javax.swing.GroupLayout.PREFERRED_SIZE, 931, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(37, 37, 37))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addComponent(HomePanel, javax.swing.GroupLayout.DEFAULT_SIZE, 691, Short.MAX_VALUE)
-                .addContainerGap())
-        );
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/MenuIMG/admin_wallpaper1.jpg"))); // NOI18N
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-200, 10, 1270, 720));
     }// </editor-fold>//GEN-END:initComponents
     private String getBuoi(){
         int hour = c.get(Calendar.HOUR_OF_DAY);
@@ -121,9 +80,9 @@ public class Dashboard extends javax.swing.JPanel {
 
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel HomePanel;
     private javax.swing.JLabel LbBuoiIcon;
     private javax.swing.JLabel LbChao;
     private javax.swing.JLabel LbNgayThangNam;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
